f2b37ad414a9a7ffe51f8e072950746c35e007c2
w00zla <chrishei83@gmail.com>
2017-04-10 15:32:48 +0200

    converted tabs to spaces

--- scripts/lib/fleetcontrol/config.lua
+++ scripts/lib/fleetcontrol/config.lua
@@ -13,76 +13,76 @@ package.path = package.path .. ";data/scripts/lib/?.lua"
 
 -- available defaults
 local defaults = {
-	debugoutput = false
+    debugoutput = false
 }
 
 local configprefix = "fleetcontrol_"
 
 local Config = {}
 local debugoutput = {
-	server = nil,
-	sector = nil,
-	player = nil,
-	entity = nil
+    server = nil,
+    sector = nil,
+    player = nil,
+    entity = nil
 }
 
 
 function Config.saveValue(scope, config, val)
 
-	local storagekey = configprefix .. config
+    local storagekey = configprefix .. config
 
-	local target = getTargetByScope(scope)
-	target:setValue(storagekey, val)
-	
+    local target = getTargetByScope(scope)
+    target:setValue(storagekey, val)
+    
 end
 
 
 function Config.loadValue(scope, config)
 
-	local storagekey = configprefix .. config
+    local storagekey = configprefix .. config
 
-	local target = getTargetByScope(scope)
-	local val = target:getValue(storagekey)
-	
-	if not val and defaults[config] then
-		val = defaults[config]
-	end
-	return val
+    local target = getTargetByScope(scope)
+    local val = target:getValue(storagekey)
+    
+    if not val and defaults[config] then
+        val = defaults[config]
+    end
+    return val
 
 end
 
 
 function getTargetByScope(scope)
 
-	if scope == "entity" then
-		return Entity()
-	elseif scope == "player" then
-		return Player()
-	elseif scope == "sector" then
-		return Sector()
-	elseif scope == "server" then
-		return Server()
-	end
+    if scope == "entity" then
+        return Entity()
+    elseif scope == "player" then
+        return Player()
+    elseif scope == "sector" then
+        return Sector()
+    elseif scope == "server" then
+        return Server()
+    end
 
 end
 
 
 function Config.getCurrent()
 
-	local cfg = {
-		-- galaxypath = Config.loadValue("galaxypath"),
-	}
-	
-	return cfg
+    local cfg = {
+        -- galaxypath = Config.loadValue("galaxypath"),
+    }
+    
+    return cfg
 end
 
 
 function Config.debugoutput(scope)
 
-	if debugoutput[scope] == nil then
-		debugoutput[scope] = Config.loadValue(scope, "debugoutput")
-	end	
-	return debugoutput[scope]
+    if debugoutput[scope] == nil then
+        debugoutput[scope] = Config.loadValue(scope, "debugoutput")
+    end	
+    return debugoutput[scope]
 
 end
 
--- scripts/player/fleetcontrol/manager.lua
+++ scripts/player/fleetcontrol/manager.lua
@@ -22,11 +22,11 @@ local storage = "notyet"
 
 function initialize()
 
-	-- subscribe to event callbacks
-	Player():registerCallback("onShipChanged", "onShipChanged")	
-	
-	addShipScript(fc_controlui) 
-	
+    -- subscribe to event callbacks
+    Player():registerCallback("onShipChanged", "onShipChanged")	
+    
+    addShipScript(fc_controlui) 
+    
 end
 
 
@@ -34,10 +34,12 @@ end
 -- given return values are persisted into galaxy database
 function secure()
 
-	if storage then
-		return storage
-	end
-	
+    scriptLog("manager.lua -> secure() called")
+
+    if storage then
+        return storage
+    end
+    
 end
 
 
@@ -45,49 +47,51 @@ end
 -- given arguments are persisted return values of secure() function 
 function restore(data)
 
-	storage = data
-	
+    scriptLog("manager.lua -> restore() called")
+
+    storage = data
+    
 end
 
 
 function onShipChanged(playerIndex, craftIndex)
 
-	-- remove and add script to ship entities
-	removeEntityScript(lastCraft, fc_controlui)	
-	addShipScript(fc_controlui)	
+    -- remove and add script to ship entities
+    removeEntityScript(lastCraft, fc_controlui)	
+    addShipScript(fc_controlui)	
 
 end
 
 
 function addShipScript(script) 
 
-	-- add script to current ship entity
-	if Player().craftIndex and Player().craftIndex > 0 then
-		local entity = Entity(Player().craftIndex)
-		if entity then
-			ensureEntityScript(entity, script)
-			lastCraft = entity.index	
-		end
-	end
+    -- add script to current ship entity
+    if Player().craftIndex and Player().craftIndex > 0 then
+        local entity = Entity(Player().craftIndex)
+        if entity then
+            ensureEntityScript(entity, script)
+            lastCraft = entity.index	
+        end
+    end
 
 end
 
 
 function removeScripts()
 
-	-- remove scripts from all ship entities
-	local currentCraft = Player().craftIndex
-	removeEntityScript(currentCraft, fc_controlui)
-	
-	if lastCraft ~= currentCraft then
-		removeEntityScript(lastCraft, fc_controlui)
-	end
+    -- remove scripts from all ship entities
+    local currentCraft = Player().craftIndex
+    removeEntityScript(currentCraft, fc_controlui)
+    
+    if lastCraft ~= currentCraft then
+        removeEntityScript(lastCraft, fc_controlui)
+    end
 
-	-- unsubscribe from event callbacks
-	Player():unregisterCallback("onShipChanged", "onShipChanged")
+    -- unsubscribe from event callbacks
+    Player():unregisterCallback("onShipChanged", "onShipChanged")
 
-	-- kill and remove script from entity
-	terminate()
-	
+    -- kill and remove script from entity
+    terminate()
+    
 end
 
